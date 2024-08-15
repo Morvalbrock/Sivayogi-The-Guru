0af1ba1adc73c9d2e3744f93de388694c3422c0c
Morvalbrock <agilansr222@gmail.com>
2024-08-15 15:59:28 +0530

    jwt error fixing

--- lib/data/api_service.dart
+++ lib/data/api_service.dart
@@ -9,16 +9,47 @@ import 'package:Sivayogi_The_Guru/model/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
   //register data
 
-  String _authToken = '';
+  // String _authToken = '';
   final String baseUrl = 'https://vgroups-api.pharma-sources.com/api/';
   final String registerEndpoint = 'user/';
   final String loginEndpoint = 'token/';
   final String userEndpoint = 'user/';
+
+  bool isTokenExpired(String token) {
+    final parts = token.split('.');
+    if (parts.length != 3) {
+      return true; // Invalid token format, consider it expired
+    }
+
+    final payload = json
+        .decode(utf8.decode(base64Url.decode(base64Url.normalize(parts[1]))));
+
+    final expiry = DateTime.fromMillisecondsSinceEpoch(payload['exp'] * 1000);
+
+    return DateTime.now().isAfter(expiry);
+  }
+
+  Future<String> getAuthToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token') ?? '';
+  }
+
+  Future<String> getValidAuthToken() async {
+    String authToken = await getAuthToken();
+
+    if (isTokenExpired(authToken)) {
+      await fetchAccess();
+      authToken = await getAuthToken();
+    }
+
+    return authToken;
+  }
 
   Future<void> register(
       RegisterModel registerModel, BuildContext context) async {
@@ -118,34 +149,67 @@ class ApiService {
     }
   }
 
+  Future<void> fetchAccess() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final authToken = await getAuthToken();
+    final uri =
+        Uri.parse('https://vgroups-api.pharma-sources.com/api/token/refresh/');
+
+    try {
+      http.Response response = await http.post(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: json.encode(authToken),
+      );
+
+      if (response.statusCode == 200) {
+        final token = json.decode(response.body);
+        prefs.setString('token', token['access']);
+        // prefs.setString('refresh_token', token['refresh']);
+      } else {
+        // Navigator.of(context).pushNamed("/");
+        print('error');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   //user data
 
   Future<UserModel> fetchUserInfo() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    _authToken = prefs.getString('token') ?? '';
+    final authToken = await getValidAuthToken();
 
     final response = await http.get(
       Uri.parse('$baseUrl$userEndpoint'),
       headers: {
-        'Authorization': 'Bearer $_authToken',
+        'Authorization': 'Bearer $authToken',
       },
     );
 
     if (response.statusCode == 200) {
       Map<String, dynamic> data = json.decode(response.body);
       return UserModel.fromJson(data);
+    } else if (response.statusCode == 403) {
+      // Handle unauthorized (possibly invalid token)
+      await logoutUser();
+      throw Exception('Unauthorized access. Please log in again.');
     } else {
       throw Exception(response.reasonPhrase);
     }
   }
 
   Future<void> uploadImageToApi(String imagePath) async {
+    final authToken = await getValidAuthToken();
     try {
       var request = http.MultipartRequest(
         'PATCH',
         Uri.parse('https://vgroups-api.pharma-sources.com/api/user/'),
       );
-      request.headers['Authorization'] = 'Bearer $_authToken';
+      request.headers['Authorization'] = 'Bearer $authToken';
       request.files.add(
         await http.MultipartFile.fromPath('profile_pic', imagePath),
       );
@@ -155,6 +219,10 @@ class ApiService {
       if (response.statusCode == 200) {
         print('Image uploaded successfully');
         await fetchUserInfo();
+      } else if (response.statusCode == 403) {
+        // Handle unauthorized (possibly invalid token)
+        await logoutUser();
+        throw Exception('Unauthorized access. Please log in again.');
       } else {
         print('Failed to upload image. Status code: ${response.statusCode}');
       }
@@ -164,6 +232,7 @@ class ApiService {
   }
 
   Future<void> updateUserData(Map<String, dynamic> updatedData) async {
+    final authToken = await getValidAuthToken();
     final uri = Uri.parse('https://vgroups-api.pharma-sources.com/api/user/');
     try {
       final response = await http.patch(
@@ -171,7 +240,7 @@ class ApiService {
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
-          'Authorization': 'Bearer $_authToken',
+          'Authorization': 'Bearer $authToken',
         },
         body: json.encode(updatedData),
       );
@@ -187,6 +256,10 @@ class ApiService {
           fontSize: 16.0,
         );
         await fetchUserInfo();
+      } else if (response.statusCode == 403) {
+        // Handle unauthorized (possibly invalid token)
+        await logoutUser();
+        throw Exception('Unauthorized access. Please log in again.');
       } else {
         Fluttertoast.showToast(
           msg: 'Failed to update',
@@ -205,8 +278,7 @@ class ApiService {
   }
 
   Future<List<dynamic>> fetchaboutInfo() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final authToken = prefs.getString('token') ?? '';
+    final authToken = await getValidAuthToken();
 
     try {
       final response = await http.get(
@@ -219,6 +291,10 @@ class ApiService {
       if (response.statusCode == 200) {
         final responseBody = utf8.decode(response.bodyBytes);
         return json.decode(responseBody) as List<dynamic>;
+      } else if (response.statusCode == 403) {
+        // Handle unauthorized (possibly invalid token)
+        await logoutUser();
+        throw Exception('Unauthorized access. Please log in again.');
       } else {
         // Handle errors by throwing an exception
         throw Exception('Failed to load about information');
@@ -231,13 +307,8 @@ class ApiService {
 
   //home page silder
 
-  Future<String> getAuthToken() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString('token') ?? '';
-  }
-
   Future<List<String>> fetchImages() async {
-    final authToken = await getAuthToken();
+    final authToken = await getValidAuthToken();
     final response = await http.get(
       Uri.parse('https://vgroups-api.pharma-sources.com/api/sliders/'),
       headers: {'Authorization': 'Bearer $authToken'},
@@ -246,6 +317,10 @@ class ApiService {
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body);
       return data.map((item) => item['image'] as String).toList();
+    } else if (response.statusCode == 403) {
+      // Handle unauthorized (possibly invalid token)
+      await logoutUser();
+      throw Exception('Unauthorized access. Please log in again.');
     } else {
       throw Exception('Failed to load images: ${response.statusCode}');
     }
@@ -254,7 +329,7 @@ class ApiService {
 //course data
 
   Future<List<Course>> fetchCourses() async {
-    final authToken = await getAuthToken();
+    final authToken = await getValidAuthToken();
     final response = await http.get(
       Uri.parse('https://vgroups-api.pharma-sources.com/api/courses/'),
       headers: {'Authorization': 'Bearer $authToken'},
@@ -264,6 +339,10 @@ class ApiService {
       final responseBody = utf8.decode(response.bodyBytes);
       List<dynamic> data = json.decode(responseBody);
       return data.map((json) => Course.fromJson(json)).toList();
+    } else if (response.statusCode == 403) {
+      // Handle unauthorized (possibly invalid token)
+      await logoutUser();
+      throw Exception('Unauthorized access. Please log in again.');
     } else {
       throw Exception('Failed to load courses: ${response.statusCode}');
     }
@@ -272,7 +351,7 @@ class ApiService {
   //books data
 
   Future<List<BookModel>> fetchBooks() async {
-    final authToken = await getAuthToken();
+    final authToken = await getValidAuthToken();
     final response = await http.get(
       Uri.parse('https://vgroups-api.pharma-sources.com/api/books/'),
       headers: {'Authorization': 'Bearer $authToken'},
@@ -282,8 +361,37 @@ class ApiService {
       final responseBody = utf8.decode(response.bodyBytes);
       List<dynamic> data = json.decode(responseBody);
       return data.map((json) => BookModel.fromJson(json)).toList();
+    } else if (response.statusCode == 403) {
+      // Handle unauthorized (possibly invalid token)
+      await logoutUser();
+      throw Exception('Unauthorized access. Please log in again.');
     } else {
       throw Exception('Failed to load books: ${response.statusCode}');
     }
   }
+}
+
+//logout
+
+Future<void> logoutUser() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.remove('token');
+  await prefs.remove('refresh_token');
+
+  // Optionally show a logout message
+  Fluttertoast.showToast(
+    msg: 'Session expired. Please log in again.',
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.CENTER,
+    timeInSecForIosWeb: 1,
+    backgroundColor: Colors.red,
+    textColor: Colors.white,
+    fontSize: 16.0,
+  );
+
+  // Navigate to the login screen and remove all other routes
+  Navigator.of(context as BuildContext).pushAndRemoveUntil(
+    MaterialPageRoute(builder: (context) => const LoginScreen()),
+    (Route<dynamic> route) => false,
+  );
 }
